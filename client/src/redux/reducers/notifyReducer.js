3814a456dd7a3372ef3fed0a7355f4dc650fc83c
ayrAtMaterGog63 <airatgafurow@yandex.ru>
2022-01-30 00:01:41 +0300

    -_-

--- client/src/redux/reducers/notifyReducer.js
+++ client/src/redux/reducers/notifyReducer.js
@@ -16,23 +16,73 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {login, logout, refreshToken, register} from "../actions/authAction";
 import {getProfileUsers} from "../actions/profileAction";
+// {
+// [login.pending]: (state) => {
+//     console.log(state)
+//     return {loading: true}
+// },
+// [login.fulfilled]: (state, action) => {
+//     console.log(state)
+//     console.log(action.payload)
+//     return {success: action.payload.msg}
+// },
+// [login.rejected]: (state, action) => {
+//     console.log(action.payload)
+//     return {...action.payload}
+// },
+// [register.pending]: (state) => {
+//     return {loading: true}
+// },
+// [register.fulfilled]: (state, action) => {
+//     return {success: action.payload.msg}
+// },
+// [register.rejected]: (state, action) => {
+//     return {...action.payload}
+// },
+// [logout.pending]: (state) => {
+//     return {loading: true}
+// },
+// [logout.rejected]: (state, action) => {
+//     return {...action.payload}
+// },
+// [refreshToken.pending]: (state) => {
+//     return {loading: true}
+// },
+// [refreshToken.fulfilled]: (state) => {
+//     return {}
+// },
+// [refreshToken.rejected]: (state, action) => {
+//     return {...action.payload}
+// },
+// [getProfileUsers.rejected]: (state, action) => {
+//     return {...action.payload}
+// }
+// // builder => {
+// //     builder.addCase(login.pending, (state, action) => {
+// //         state = {loading: true}
+// //     })
+// // }
 
 const notifySlice = createSlice({
     name: 'alert',
     initialState: {},
     reducers: {
-        alert(state, action){
+        alert(state, action) {
             state = action.payload
         }
     },
     extraReducers: {
         [login.pending]: (state) => {
+            console.log(state)
             state = {loading: true}
         },
         [login.fulfilled]: (state, action) => {
+            console.log(state)
+            console.log(action.payload)
             state = {success: action.payload.msg}
         },
         [login.rejected]: (state, action) => {
+            console.log(action.payload)
             state = action.payload
         },
         [register.pending]: (state) => {
